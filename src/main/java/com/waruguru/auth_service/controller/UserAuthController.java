b88da96473842daf1af30ca8280d780b3a7748e7
waruguru <rachelmuriithi57@gmail.com>
2026-03-09 17:02:56 +0300

    Update logging aand connect to postgress

--- src/main/java/com/waruguru/auth_service/controller/UserAuthController.java
+++ src/main/java/com/waruguru/auth_service/controller/UserAuthController.java
@@ -20,28 +20,28 @@ public class UserAuthController {
     private final UserAuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse<UserAuthResponse>> register(@RequestBody UserRegisterRequest request) {
+    public ResponseEntity<ApiResponse<String>> register(@RequestBody UserRegisterRequest request) {
         String token = String.valueOf(authService.registerUser(request));
         UserAuthResponse response = new UserAuthResponse(token);
 
-        ApiResponse<UserAuthResponse> userAuthResponseApiResponse = new ApiResponse<>(
+        ApiResponse<String> userAuthResponseApiResponse = new ApiResponse<>(
                 HttpStatus.CREATED.value(),
                  "Registration successful",
                 null,
-                response);
+                response.getToken());
 
         return ResponseEntity.ok(userAuthResponseApiResponse);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<UserAuthResponse>> login(@RequestBody UserLoginRequest request) {
+    public ResponseEntity<ApiResponse<String>> login(@RequestBody UserLoginRequest request) {
         UserAuthResponse response = authService.loginUser(request);
 
-        ApiResponse<UserAuthResponse> apiResponse = new ApiResponse<>(
+        ApiResponse<String> apiResponse = new ApiResponse<>(
                 HttpStatus.OK.value(),
                 "Login successful",
                 null,
-                response
+                response.getToken()
         );
 
         return ResponseEntity.ok(apiResponse);
